b0070a162d99c51841eb614d2b875010955fc2f5
41486 <41486@users.noreply.github.com>
2015-10-05 17:55:59 +0200

    main update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    Game* game = new Game;
+
+    w.setGame(game);
+
+    delete game;
 
     return a.exec();
 }
